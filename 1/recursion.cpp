34118943448790b03eaebc411598835a5f44d9bb
jjjjjjyt <wildlike@163.com>
2024-05-26 23:06:04 +0800

    2024-05-26.1

--- 1/recursion.cpp
+++ 1/recursion.cpp
@@ -1,2 +1,25 @@
-//1
-//
+//等差数列
+//f(n) = f(n-1) + 3
+
+#include <iostream>
+using namespace std;
+
+int fun(int n)
+{
+    int res;
+    if (n == 1) {
+        return 1;
+    }
+ 
+    res = fun(n - 1) + 3;
+
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout <<  fun(n) << endl;
+    return 0;
+}
